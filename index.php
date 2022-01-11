b2463fb01e7a36ce1464e1fd419d0d44e55bd36f
Gyuri21 <gyuri21hegedus@gmail.com>
2022-01-11 12:05:27 +0100

    html-be kiirja az adatokat

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
 <?php
 include_once('bookModel.php');
 $data = bookModel::getBooks();
-print_r($data);
+
+echo file_get_contents('templates/head.html');
+echo file_get_contents('templates/foot.html');
+$rowtpl = file_get_contents('templates/row.html');
+$content = '';
+foreach($data as $book){
+    $row = $rowtpl;
+    $row = str_replace('{id}',$book->id,$row);
+    $row = str_replace('{title}',$book->title,$row);
+    $row = str_replace('{writer}',$book->writer,$row);
+    $content = $content . $row;
+}
+
+$page = file_get_contents('templates/index.html');
+$page = str_replace('{content}',$content,$page);
+echo $page;
+
+
+
+
+
+
